d46662271596f3ba406b1079de0c17ddc6b529e2
gaurav3210 <gs070599@gmail.com>
2020-05-29 03:16:37 +0530

    Added app.js server and get method

---
+++ app.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var items = ["Buy Food","Cook Food","Eat Food"];
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.get("/",function(req,res){
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleString("en-US",options);
+  res.render("list",{kindOfDay:day, newListItems:items});
+
+
+});
+
+app.listen(3000,function(){
+  console.log("Server started on port 3000");
+});
