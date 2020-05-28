0eff80b52f0ca8411c9595fea75e85bdf775941d
gaurav3210 <gs070599@gmail.com>
2020-05-29 04:26:56 +0530

    Added work page for todolist

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 var items = ["Buy Food","Cook Food","Eat Food"];
+var workItems = [];
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -19,7 +20,7 @@ app.get("/",function(req,res){
     month: "long"
   };
   var day = today.toLocaleString("en-US",options);
-  res.render("list",{kindOfDay:day, newListItems:items});
+  res.render("list",{listTitle:day, newListItems:items});
 
 
 });
@@ -30,6 +31,15 @@ app.post("/",function(req,res){
  res.redirect("/");
 });
 
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work",newListItems:workItems});
+});
+
+app.post("/work",function(req,res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 app.listen(3000,function(){
   console.log("Server started on port 3000");
 });
