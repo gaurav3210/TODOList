105023a964a3140d88f7191639a91a2892ef5fdc
gaurav3210 <gs070599@gmail.com>
2020-05-30 02:57:09 +0530

    Data base got connected.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname+"/day.js");
 const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -10,12 +9,40 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemSchema = new mongoose.Schema({
+   name:{
+     type:String,
+     required:true
+   }
+});
+
+const Item = mongoose.model("Item",itemSchema);
+
+const item1 = new Item({
+name:"BuyFood"
+});
+const item2 = new Item({
+  name:"cookFood"
+});
+const item3 = new Item({
+  name:"eatFood"
+});
+
+const predefinedItems = [item1,item2,item3];
+
+Item.insertMany(predefinedItems,function (err) {
+  if(err)
+  {
+    console.log("ERROR OCCURRED");
+  }else{
+    console.log("Successfully implemented");
+  }
+
+})
 app.get("/",function(req,res){
 
-  let day = date.getDate();
-  res.render("list",{listTitle:day, newListItems:items});
-
-
+  res.render("list",{listTitle:"Today", newListItems:predefinedItems});
 });
 
 app.post("/",function(req,res){
@@ -33,16 +60,7 @@ app.get("/work",function(req,res){
   res.render("list",{listTitle:"Work",newListItems:workItems});
 });
 
-app.post("/work",function(req,res){
 
-  let item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
-})
-
-app.get("/about",function(req,res){
-  res.render("about");
-})
 app.listen(3000,function(){
   console.log("Server started on port 3000");
 });
