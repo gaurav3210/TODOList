f4ccf43c4664be7a2d00b749d458c2c0912456c0
gaurav3210 <gs070599@gmail.com>
2021-09-04 05:03:43 +0530

    Minor changes

--- app.js
+++ app.js
@@ -132,6 +132,7 @@ app.get("/:customListName",function (req,res) {
 
 })
 
-app.listen(3000,function(){
-  console.log("Server started on port 3000");
+const PORT = 3000;
+app.listen(PORT,function(){
+  console.log("Server started on port ${PORT}");
 });
